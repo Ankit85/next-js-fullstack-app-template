2f11484e148c847fa068ab9e7d2862a9bc284980
Ankit <ankit60.av@gmail.com>
2023-01-01 00:01:21 +0530

    build:updated Index page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div className="bg-red-600">
-      <p>hello</p>
+    <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500">
+      <div>
+        <h1 className="text-xl">
+          This is next js Full stack Project configure with below stack
+        </h1>
+        <ul>
+          <li>Next Js</li>
+          <li>Tailwind</li>
+          <li>Husk with precomit and pre push script</li>
+          <li>Prettier</li>
+          <li>Eslint</li>
+        </ul>
+      </div>
     </div>
   );
 }
